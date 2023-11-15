ac19183c405124cdf994d8e7e5692cf544ec4e3e
Harsha <101426063+JalaHarshavardhan@users.noreply.github.com>
2023-11-15 07:14:24 +0530

    insert-chat.php

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -1,16 +1,36 @@
 <?php
-    session_start();
-    if(isset($_SESSION['unique_id'])){
-        include_once "config.php";
-        $outgoing_id = mysqli_real_escape_string($conn, $_POST['outgoing_id']);
-        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
-        $message = mysqli_real_escape_string($conn, $_POST['message']);
+session_start();
+if (isset($_SESSION['unique_id'])) {
+    include_once "config.php";
+    $outgoing_id = mysqli_real_escape_string($conn, $_POST['outgoing_id']);
+    $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+    $message = mysqli_real_escape_string($conn, $_POST['message']);
 
-        if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
-                                VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
+    // Retrieve the sender's IP addresses
+    $sender_ip = getRealIP();
+
+    if (!empty($message)) {
+        $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg, sender_ip)
+                            VALUES ({$incoming_id}, {$outgoing_id}, '{$message}', '{$sender_ip}')") or die();
+        if ($sql) {
+            echo "success";
+        } else {
+            echo "Error inserting message.";
         }
-    }else{
-        header("/login.php");
     }
+} else {
+    header("/login.php");
+}
+
+// Function to get the real IP address
+function getRealIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
 ?>
